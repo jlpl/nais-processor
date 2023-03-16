4768f623432191ce549a1335965fdbfec0a78a03
jlpl <janne.lampilahti@gmail.com>
2023-03-16 11:56:06 +0200

    Fix import error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setup(
     name="nais-processor",
-    version="0.0.19",
+    version="0.0.20",
     description='Code to process ion spectrometer data files',
     py_modules=["nais_processor"],
     package_dir={'':'src'},
